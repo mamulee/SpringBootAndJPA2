964757baf4173e5b0ff348527ec5fd38ee09f5de
mamulee <ghdah7@gmail.com>
2022-08-01 23:28:23 +0900

    간단한 주문 조회 V4

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
@@ -27,7 +28,7 @@ import static java.util.stream.Collectors.*;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
-
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
     // Entity 직접 노출
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
@@ -62,6 +63,16 @@ public class OrderSimpleApiController {
                 .map(o -> new SimpleOrderDto(o))
                 .collect(toList());
         return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        // 역시 쿼리가 딱 1번 나감
+        // 더불어 내가 원하는 컬럼만 선정해서 select 해온다.
+        // 하지만 재사용성이 떨어지는 단점이 있다.
+        // OrderRepository 내에 메소드를 생성하면 불편해질 수 있기 때문에 따로 분리하는 것 추천
+        // -> 그래서 repository 패키지 내에 order.simplequery 패키지 안으로 옮김.
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
     @Data
